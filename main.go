bec58566f475ac76f7564c5b9875560ae8216fdc
dragosguta <contact.dragosguta@gmail.com>
2019-07-04 17:58:29 -0700

    dg: read port from env

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main() {
 	}
 
 	http.Handle("/", buildChain(http.HandlerFunc(handleRequest), mware...))
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
 	}
 }
